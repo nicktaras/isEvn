5ce5241eef847bf4a49bcb027afe47207486c14f
Nick Taras <nicktaras@hotmail.co.uk>
2019-05-21 11:28:56 +1000

    Updated test
    Updated test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const tap = require('tap');
 const isEvn = require("./../index");
 
-tap.equal(isEvn(1), true)
-tap.equal(isEvn(2), false)
+tap.equal(isEvn(1), false)
+tap.equal(isEvn(2), true)
 tap.equal(isEvn('foo'), false)
 tap.equal(isEvn('/'), false)
