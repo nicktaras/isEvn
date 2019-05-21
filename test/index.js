070e34283687a1f8dd8fc98c830dc92af9065b6b
Nick Taras <nicktaras@hotmail.co.uk>
2019-05-21 14:06:14 +1000

    adjusted warning logic and added further tests to ensure correct functionality.

--- test/index.js
+++ test/index.js
@@ -3,5 +3,10 @@ const isEvn = require("./../index");
 
 tap.equal(isEvn(1), false)
 tap.equal(isEvn(2), true)
+tap.equal(isEvn(20190), true)
+tap.equal(isEvn(2019111111), false)
 tap.equal(isEvn('foo'), false)
 tap.equal(isEvn('/'), false)
+tap.equal(isEvn({}), false)
+tap.equal(isEvn(undefined), false)
+tap.equal(isEvn(null), false)
